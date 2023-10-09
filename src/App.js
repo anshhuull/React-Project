454c110f1f58febc3fbe68203edf16a3ff82bebc
anshhuull <ansh770919@gmail.com>
2023-10-09 16:42:04 +0530

    Upadates

--- src/App.js
+++ src/App.js
@@ -1,49 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Household Expense",
-      amount: 25000,
-      date: new Date(2023, 4, 27),
-    },
-    {
-      id: "e2",
-      title: "Travel Expense",
-      amount: 1300,
-      date: new Date(2023, 5, 28),
-    },
-    {
-      id: "e3",
-      title: "Education fees",
-      amount: 5000,
-      date: new Date(2023, 6, 29),
-    },
-  ];
+const TEST_EXPENSES = [
+  {
+    id: "e1",
+    title: "Household Expense",
+    amount: 25000,
+    date: new Date(2023, 4, 27),
+  },
+  {
+    id: "e2",
+    title: "Travel Expense",
+    amount: 1300,
+    date: new Date(2023, 5, 28),
+  },
+  {
+    id: "e3",
+    title: "Education fees",
+    amount: 5000,
+    date: new Date(2023, 6, 29),
+  },
+];
 
-  const addExpenseHandler= expense =>{
-    console.log('In App.js');
-    console.log(expense)
-  }
-  
+const App = () => {
+  const [expenses, setExpenses] = useState(TEST_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+
+      <Expenses items={expenses} />
     </div>
-  )
+  );
 };
-
-
-// return React.createElement(
-//   "div",
-//   {},
-//   React.createElement("h2", {}, "Hello Coders!"),
-//   React.createElement(Expenses, { items: expenses })
-// );
-
 
 export default App;
