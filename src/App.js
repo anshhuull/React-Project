3d6c60d3494e6e4b0c35944c03cfe3b06b932bfb
anshhuull <ansh770919@gmail.com>
2023-10-09 18:15:38 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
@@ -8,20 +7,38 @@ const TEST_EXPENSES = [
     id: "e1",
     title: "Household Expense",
     amount: 25000,
-    date: new Date(2023, 4, 27),
+    date: new Date(2023, 4, 27)
   },
   {
     id: "e2",
     title: "Travel Expense",
     amount: 1300,
-    date: new Date(2023, 5, 28),
+    date: new Date(2023, 5, 28)
   },
   {
     id: "e3",
     title: "Education fees",
     amount: 5000,
-    date: new Date(2023, 6, 29),
+    date: new Date(2023, 6, 29)
   },
+  {
+    id: "e4",
+    title: "Car",
+    amount: 450000,
+    date: new Date(2022, 8, 20)
+  },
+  {
+    id: "e5",
+    title: "MotorBike",
+    amount: 73000,
+    date: new Date(2022, 4, 15)
+  },
+  {
+    id: "e6",
+    title: "Mobile Phone",
+    amount: 15000,
+    date: new Date(2021, 7, 24)
+  }
 ];
 
 const App = () => {
@@ -36,7 +53,6 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-
       <Expenses items={expenses} />
     </div>
   );
